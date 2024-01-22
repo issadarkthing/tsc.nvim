ded20ab038d0f57aa1d36cdf759cd7fa31704c81
raziman <raziman.terra@gmail.com>
2024-01-22 14:56:07 +0800

    change sensible defaults

--- lua/tsc/init.lua
+++ lua/tsc/init.lua
@@ -11,17 +11,17 @@ end
 
 local DEFAULT_CONFIG = {
   auto_open_qflist = true,
-  auto_close_qflist = false,
-  auto_focus_qflist = false,
-  auto_start_watch_mode = false,
+  auto_close_qflist = true,
+  auto_focus_qflist = true,
+  auto_start_watch_mode = true,
   bin_path = utils.find_tsc_bin(),
-  enable_progress_notifications = true,
+  enable_progress_notifications = false,
   flags = {
     noEmit = true,
     project = function()
       return utils.find_nearest_tsconfig()
     end,
-    watch = false,
+    watch = true,
   },
   hide_progress_notifications_from_history = true,
   spinner = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
